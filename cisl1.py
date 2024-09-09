728944764af0657938099443de7f988370fcee27
gastavoon <151585262+gastavoon@users.noreply.github.com>
2024-09-09 16:45:01 +0300

    Update and rename cisl1 to cisl8.py

---
+++ cisl1.py
@@ -0,0 +1,13 @@
+x = int(input(""))
+y = int(input(""))
+
+if x>30 and y>30:
+    print("Слишком большие числа , попробуйте меньше")
+else:
+    print(x+y)
+    print(x-y)
+    print(x/y)
+    print(x**y)
+    print(x%y)
+    print(x//y)
+    print(x*y)
